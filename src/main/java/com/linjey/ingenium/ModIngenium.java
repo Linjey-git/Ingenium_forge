188d485eb77e47d82c68b9af6b3d89af251d69c3
Linjey-git <linjey.ubi@gmail.com>
2022-01-01 22:13:58 +0200

    Test commit

--- src/main/java/com/linjey/ingenium/ModIngenium.java
+++ src/main/java/com/linjey/ingenium/ModIngenium.java
@@ -31,6 +31,7 @@ import java.util.stream.Collectors;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ModIngenium.MOD_ID)
 public class ModIngenium {
+    //test
 
     public static final String MOD_ID = "ingenium";
     // Directly reference a log4j logger.
